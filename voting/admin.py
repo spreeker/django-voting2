7ee507e523d230a0f70ef01e13d44a86037ed381
Stephan Preeker <stephan@preeker.net>
2009-09-12 15:34:19 +0200

    removed some paretn project code

--- voting/admin.py
+++ voting/admin.py
@@ -1,23 +1,10 @@
 from django.contrib import admin
 
-from issue.models import Issue
 from models import Vote
-
-class IssueAdmin(admin.ModelAdmin):
-    date_hierarchy = "time_stamp"
-    list_display = ('title', 'time_stamp', 'user', 'votes' ) 
-    list_filter = ('title', 'time_stamp', 'user', 'votes' )
-
-    def votes(self , obj):
-        return Vote.objects.get_for_object(obj).count()
 
 class VoteAdmin(admin.ModelAdmin):
     date_hierarchy = "time_stamp"
     list_display = ('vote', 'issue_title', 'user', 'time_stamp')
     list_filter = ('user', 'time_stamp', )
 
-    def issue_title(self , obj):
-        return ("%s" % obj.payload.title) 
-    
-admin.site.register(Issue , IssueAdmin)
 admin.site.register(Vote, VoteAdmin)
