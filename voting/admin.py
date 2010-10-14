bd6b109e498ae09ba892880738183188757c10b6
Philipp Wassibauer <phil@maptales.com>
2010-10-14 05:36:12 -0700

    issue_title is not a field

--- voting/admin.py
+++ voting/admin.py
@@ -4,7 +4,7 @@ from models import Vote
 
 class VoteAdmin(admin.ModelAdmin):
     date_hierarchy = "time_stamp"
-    list_display = ('vote', 'issue_title', 'user', 'time_stamp')
+    list_display = ('vote', 'user', 'time_stamp')
     list_filter = ('user', 'time_stamp', )
 
 admin.site.register(Vote, VoteAdmin)
